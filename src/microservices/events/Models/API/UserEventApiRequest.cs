f0d8daa16d2cc48d662e386b91b23669f4806928
VolodinAndrey <volodin33@gmail.com>
2026-01-24 20:30:06 +0300

    Ingres + proxy + tests fix

--- src/microservices/events/Models/API/UserEventApiRequest.cs
+++ src/microservices/events/Models/API/UserEventApiRequest.cs
@@ -1,8 +1,12 @@
-﻿namespace events.Models.API;
+﻿using System.Text.Json.Serialization;
+
+namespace events.Models.API;
 
 public class UserEventApiRequest : IEventRequest
 {
+    [JsonPropertyName("user_id")]
     public int UserId { get; init; }
+    
     public string? Username { get; init; }
     public string? Email { get; init; }
     public string Action { get; init; } = default!;
